57fc395859585c7c37fef19cfcc890572d55bfd9
yer <yer@hotech.systems>
2019-07-11 17:39:50 +0300

    Updates

---
+++ precache-manifest.0212c03b801694c800bc0bbc1937bca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e0be74165216c534d05197218b06a8",
+    "url": "/myReactApp/index.html"
+  },
+  {
+    "revision": "f8b0f990c2a06c4b696c",
+    "url": "/myReactApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/myReactApp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f8b0f990c2a06c4b696c",
+    "url": "/myReactApp/static/js/main.93d88b7f.chunk.js"
+  },
+  {
+    "revision": "a1eb4b8f904e0d6cb461",
+    "url": "/myReactApp/static/js/runtime~main.f7045292.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
